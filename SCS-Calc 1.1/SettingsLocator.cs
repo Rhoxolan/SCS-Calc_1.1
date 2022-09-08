0c9833b6d2f0e18c7b803fca220581e49cff139c
Rhoxolan <pba17mail@gmail.com>
2022-09-08 13:46:42 +0300

    Add to SettingsLocator

--- SCS-Calc 1.1/SettingsLocator.cs
+++ SCS-Calc 1.1/SettingsLocator.cs
@@ -4,16 +4,12 @@
     {
         public double GetMinPermanentLink(double minPermanentLink);
         public double GetMaxPermanentLink(double maxPermanentLink);
+        public double GetTechnologicalReserve(double technologicalReserve);
     }
 
     public interface IAnArbitraryNumberOfPorts
     {
         public int GetNumberOfPorts(int numberOfPorts);
-    }
-
-    public interface ITechnologicalReserve
-    {
-        public double GetTechnologicalReserve(double technologicalReserve);
     }
 
     public class StrictСomplianceWithTheStandart : IStrictСomplianceWithTheStandart
@@ -45,6 +41,19 @@
             }
             return minPermanentLink;
         }
+
+        public double GetTechnologicalReserve(double technologicalReserve)
+        {
+            if (technologicalReserve > 2)
+            {
+                throw new Exception("Превышено допустимое значение технологического запаса (2.00).");
+            }
+            if (technologicalReserve < 1)
+            {
+                throw new Exception("Значение технологического запаса ниже допустимого (1.00)");
+            }
+            return technologicalReserve;
+        }
     }
 
     public class NonStrictСomplianceWithTheStandart : IStrictСomplianceWithTheStandart
@@ -73,6 +82,19 @@
                 throw new Exception("Значение наибольшей длины постоянного линка ниже допустимого");
             }
             return minPermanentLink;
+        }
+
+        public double GetTechnologicalReserve(double technologicalReserve)
+        {
+            if (technologicalReserve > 10)
+            {
+                throw new Exception("Превышено допустимое значение технологического запаса (10.00).");
+            }
+            if (technologicalReserve < 1)
+            {
+                throw new Exception("Значение технологического запаса ниже допустимого (1.0)");
+            }
+            return technologicalReserve;
         }
     }
 
@@ -111,5 +133,67 @@
 
     public class SettingsLocator
     {
+        private IStrictСomplianceWithTheStandart? complianceWithTheStandart;
+        private IAnArbitraryNumberOfPorts? numberOfPorts;
+        private double technologicalReserve;
+
+        public SettingsLocator()
+        {
+            complianceWithTheStandart = null;
+            numberOfPorts = null;
+            technologicalReserve = 1;
+        }
+
+        public double TechnologicalReserve
+        {
+            set
+            {
+                technologicalReserve = value;
+            }
+        }
+
+        public void SetStrictСomplianceWithTheStandart()
+        {
+            complianceWithTheStandart = new StrictСomplianceWithTheStandart();
+        }
+
+        public void SetNonStrictСomplianceWithTheStandart()
+        {
+            complianceWithTheStandart = new NonStrictСomplianceWithTheStandart();
+        }
+
+        public void SetNotAnArbitraryNumberOfPorts()
+        {
+            numberOfPorts = new NotAnArbitraryNumberOfPorts();
+        }
+
+        public void SetAnArbitraryNumberOfPorts()
+        {
+            numberOfPorts = new AnArbitraryNumberOfPorts();
+        }
+
+        public (double MinPermanentLink, double MaxPermanentLink, double AveragePermanentLink, int NumberOfWorkplaces, int NumberOfPorts, double? СableQuantity,
+            double? CableHankMeterage, int? HankQuantity, double TotalСableQuantity)
+            Calculate(double minPermanentLink, double maxPermanentLink, int numberOfWorkplaces, int numberOfPorts, double? cableHankMeterage)
+        {
+            if(cableHankMeterage != null)
+            {
+                double TechnologicalReserve = complianceWithTheStandart!.GetTechnologicalReserve(technologicalReserve);
+                double MinPermanentLink = complianceWithTheStandart!.GetMinPermanentLink(minPermanentLink);
+                double MaxPermanentLink = complianceWithTheStandart!.GetMaxPermanentLink(maxPermanentLink);
+                double AveragePermanentLink = (MinPermanentLink + MaxPermanentLink) / 2 * TechnologicalReserve;
+                int NumberOfWorkplaces = numberOfWorkplaces;
+                int NumberOfPorts = this.numberOfPorts!.GetNumberOfPorts(numberOfPorts);
+                double? CableHankMeterage = cableHankMeterage;
+                double СableQuantity = AveragePermanentLink * NumberOfWorkplaces * NumberOfPorts;
+                int HankQuantity = (int)Math.Ceiling(NumberOfWorkplaces * NumberOfPorts / Math.Floor((double)(CableHankMeterage / AveragePermanentLink)));
+                double TotalСableQuantity = (double)(HankQuantity * CableHankMeterage);
+                return (MinPermanentLink, MaxPermanentLink, AveragePermanentLink, NumberOfWorkplaces, NumberOfPorts, СableQuantity, CableHankMeterage, HankQuantity, TotalСableQuantity);
+            }
+            else
+            {
+                return new();
+            }
+        }
     }
 }
